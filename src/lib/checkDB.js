3f50cc2078a503838693b36ab376ad390f731757
bochrap <131978926+bochrap@users.noreply.github.com>
2024-02-19 14:17:59 +0000

    get the word from the db function

--- src/lib/checkDB.js
+++ src/lib/checkDB.js
@@ -2,15 +2,21 @@
 
 import { sql } from "@vercel/postgres";
 
-export default async function checkDB(guess) {
-
+export async function checkDB(guess) {
   try {
     const isAllowedGuess = await sql`SELECT * FROM words WHERE word=${guess.toLowerCase()}`;
     return isAllowedGuess;
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
     return false;
   }
+}
 
+export async function getTheWord() {
+  // number of solutions: 2315
+  // NUMBER OF SOLUTIONS = await sql`SELECT COUNT(*) FROM words WHERE issolution='TRUE'`;
+  const generateWordId = Math.floor(Math.random() * 2315) + 1;
+  const fetchTheWord = await sql`SELECT * FROM words WHERE id=${generateWordId}`;
+  const wordString = fetchTheWord.rows[0].word;
+  return wordString;
 }
