c797fecf9ce9d452cee3a924a5c71aa4f1033a8b
II-Paulus-II <paulus.coding@gmail.com>
2024-02-20 10:35:06 +0000

    nice useeffect code not working

--- src/app/(main)/play/page.jsx
+++ src/app/(main)/play/page.jsx
@@ -1,14 +1,21 @@
 "use client";
 import Keyboard from "@/components/Keyboard";
 import Display from "@/components/display";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useGameContext } from "@/context/game-context";
+import { checkGame } from "@/lib/checkDB";
+import { getUserId } from "@/lib/users";
 
 
 export default function PlayPage() {
     const {startNewGame} = useGameContext ();
+    const smartUseEffect = useRef(false);
     useEffect(() => {
       startNewGame();
+      if (smartUseEffect.current) {
+        return
+      }
+      smartUseEffect.current = true;
     }, []);
   return (
     <div>
