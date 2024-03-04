53b481a05d522289907dd20b2a7d09dc57446f7b
bochrap <131978926+bochrap@users.noreply.github.com>
2024-03-04 09:47:47 +0000

    seed the database with words

--- words-table-schema.js
+++ words-table-schema.js
@@ -3,44 +3,40 @@ const WARNING = "To run this file you need to copy the file .env.local to .env";
 
 /* ----- Third Party Imports ----- */
 import fs from "fs";
-import 'dotenv/config';
+import "dotenv/config";
 import { sql } from "@vercel/postgres";
 
 // Demonstrate connection to database
-async function getPosts() {
-  const test = await sql`
-  SELECT * FROM test;
-`;
-  console.log("test is ", test)
-}
-//getPosts();
+// async function getPosts() {
+//   const test = await sql`
+//   SELECT * FROM test;
+// `;
+//   console.log("test is ", test);
+// }
+// getPosts();
 
 async function seedWords() {
-  const solutionsArray = fs.readFileSync('./data/wordle-La.txt').toString().split("\n");
-  const guessesArray = fs.readFileSync('./data/wordle-Ta.txt').toString().split("\n");
+  const solutionsArray = fs.readFileSync("./data/wordle-La.txt").toString().split("\n");
+  const guessesArray = fs.readFileSync("./data/wordle-Ta.txt").toString().split("\n");
   let query = `INSERT INTO words (word, isSolution) VALUES`;
-  // solutionsArray.forEach(async function(solution, index) {
+  // solutionsArray.forEach(async function (solution, index) {
   //   if (index == 0) {
-  //     query = query + `\n(\'${solution}\', TRUE)`
-  //   }
-  //   else if (index < solutionsArray.length - 1){
-  //     query = query + `,\n(\'${solution}\', TRUE)`
-  //   }
-  //   else {
-  //     query = query + `,\n(\'${solution}\', TRUE);`
+  //     query = query + `\n(\'${solution}\', TRUE)`;
+  //   } else if (index < solutionsArray.length - 1) {
+  //     query = query + `,\n(\'${solution}\', TRUE)`;
+  //   } else {
+  //     query = query + `,\n(\'${solution}\', TRUE);`;
   //   }
   // });
-  //console.log(query);
-  //const insert = await sql.query(query);
-  guessesArray.forEach(async function(guess, index) {
+  // console.log(query);
+  // const insert = await sql.query(query);
+  guessesArray.forEach(async function (guess, index) {
     if (index == 0) {
-      query = query + `\n(\'${guess}\', FALSE)`
-    }
-    else if (index < guessesArray.length - 1){
-      query = query + `,\n(\'${guess}\', FALSE)`
-    }
-    else {
-      query = query + `,\n(\'${guess}\', FALSE);`
+      query = query + `\n(\'${guess}\', FALSE)`;
+    } else if (index < guessesArray.length - 1) {
+      query = query + `,\n(\'${guess}\', FALSE)`;
+    } else {
+      query = query + `,\n(\'${guess}\', FALSE);`;
     }
   });
   console.log(query);
